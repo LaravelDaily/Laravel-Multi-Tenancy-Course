65dae176dcb881e4673c2df84ee5b5a36a5e5be1
PovilasKorop <povilask007@gmail.com>
2022-05-05 15:25:25 +0300

    Composer and config updates

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => '.' . env('SESSION_DOMAIN', 'tenancy.test'),
+    'domain' => '.' . env('SESSION_DOMAIN', 'project3.test'),
 
     /*
     |--------------------------------------------------------------------------
--- config/tenancy.php
+++ config/tenancy.php
@@ -16,7 +16,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'tenancy.test',
+        'project3.test',
     ],
 
     /**
