3825d0a9f199eafa6509d4cca3f985caecfe8f87
PovilasKorop <povilask007@gmail.com>
2022-05-06 09:44:23 +0300

    Small fix

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,7 +16,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'project3.test',
+        'tenancy.test',
     ],
 
     /**
